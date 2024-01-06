cbd325639a0f32bc3e306d01c13cb03560b128ae
megansin <megan_sin@student.wayland.k12.ma.us>
2024-01-05 19:16:25 -0500

    added project tiles

--- lib/main.dart
+++ lib/main.dart
@@ -5,135 +5,44 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/gestures.dart';
 
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have clicked the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
-
 void main() {
   runApp(MyApp());
 }
+
+class Project {
+  final String title;
+  final String description;
+
+  Project({required this.title, required this.description});
+}
+
+final List<Project> projects = [
+  Project(
+    title: 'AI Resume Parser',
+    description: 'Web application where users can upload their resume and paste a job description to see how well their resume fits the keywords found in the job description',
+  ),
+  Project(
+    title: 'SpotBot: Fake Yelp Review Analyzer',
+    description: 'Tool that utilizes machine learning to attempt to classify Yelp reviews as either fraudulent or valid',
+  ),
+  Project(
+    title: 'Elite Matchmaker: F1 Driver Sponsorship Tool',
+    description: 'Ranked and scored F1 drivers based on factors in alignment with company’s interests',
+  ),
+  Project(
+    title: 'Data for Good Hackathon: Star House Expansion Plan',
+    description: 'Recommended locations based on predicted need for nonprofit Star House’s services',
+  ),
+  Project(
+    title: 'WiDS Datathon: Diabetes Prediction',
+    description: 'Utilized machine learning to predict if patients have diabetes',
+  ),
+  Project(
+    title: 'Predictive Modeling for Stroke Diagnosis',
+    description: 'Determined risk factors for stroke prediction',
+  ),
+  // Add more projects
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -397,33 +306,80 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
-class ProjectsPage extends StatelessWidget {
+class ProjectDetailsPage extends StatelessWidget {
+  final Project project;
+
+  ProjectDetailsPage({required this.project});
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text('You have '
-              '${appState.favorites.length} favorites:'),
-        ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title: Text(pair.asLowerCase),
-          ),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(project.title),
+      ),
+      body: Center(
+        child: Text(project.description),
+      ),
     );
   }
 }
+
+class ProjectsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Theme(
+      data: ThemeData.from(
+        colorScheme: colorScheme,
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('My Projects',
+          style: GoogleFonts.firaMono(color: Colors.black, fontWeight: FontWeight.bold),),
+        ),
+        body: GridView.extent(
+          padding: EdgeInsets.symmetric(horizontal: 50.0),
+          maxCrossAxisExtent: 400,
+          crossAxisSpacing: 80,
+          mainAxisSpacing: 100,
+          children: List.generate(
+            projects.length,
+            (index) => InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProjectDetailsPage(project: projects[index]),
+                  ),
+                );
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(projects[index].title, 
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.firaMono(fontSize: 16, color: Colors.black, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 30),
+                      Text(projects[index].description, 
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.firaMono(fontSize: 14, color: Colors.black, fontWeight: FontWeight.normal),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 class SecretLinkPage extends StatelessWidget {
   @override
